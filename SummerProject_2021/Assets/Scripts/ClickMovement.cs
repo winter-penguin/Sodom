8b620f33bd6698d9b28e4fd2e12a20ce0e6987cb
WIN10PC\User <chahg0000@gmail.com>
2021-07-10 14:35:37 +0900

    CHG_ClickMove_7/10

--- SummerProject_2021/Assets/Scripts/ClickMovement.cs
+++ SummerProject_2021/Assets/Scripts/ClickMovement.cs
@@ -8,30 +8,52 @@ public class ClickMovement : MonoBehaviour
 
     private bool isMove;
     private Vector3 destination;
+    public float speed = 5;
 
-    private void Awake()
-    {
-        camera1 = Camera.main;
-    }
+    public Rigidbody rb;
+
+    Vector3 MousePosition;
+
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetMouseButton(0))
         {
-            RaycastHit hit;
-            if(Physics.Raycast(camera1.ScreenPointToRay(Input.mousePosition), out hit))
+            MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            destination = new Vector3(MousePosition.x, transform.position.y,transform.position.z);
+            Debug.Log(destination);
+
+            isMove = true; //나중에 애니메이션 할때 필요하겠죠..?
+        }
+        if (isMove && transform.position != destination)
+        {
+            
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+            /*
+            if(transform.position.x > destination.x)
             {
-                SetDestination(hit.point);
+                rb.AddForce(new Vector3(-1, 0, 0));
             }
+            else rb.AddForce(new Vector3(1, 0, 0));
+            */
+
+        }
+        else
+        {
+            isMove = false;
         }
 
-        Move();
-    }
 
+        //Move();
+    }
+    /*
     private void SetDestination(Vector3 dest)
     {
         destination = dest;
@@ -51,4 +73,5 @@ public class ClickMovement : MonoBehaviour
             isMove = false;
         }
     }
+    */
 }
