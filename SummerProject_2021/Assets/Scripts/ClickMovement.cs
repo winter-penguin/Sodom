560f215a4496f20bb09646c9542ae532e84cd229
WIN10PC\User <chahg0000@gmail.com>
2021-07-19 22:36:09 +0900

    CHG_CharacterMove_2021.07.19

--- SummerProject_2021/Assets/Scripts/ClickMovement.cs
+++ SummerProject_2021/Assets/Scripts/ClickMovement.cs
@@ -1,77 +1,99 @@
+//*******************************
+// Editor:CHG
+// LAST EDITED DATE : 2021.07.19
+// Script Purpose : Character_ClickMove
+//*******************************
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickMovement : MonoBehaviour
 {
+    [System.Serializable] // class를 인스펙터에서 보여줌
+    //[SerializeField] // private 변수를 인스펙터에서 보여줌
+    public class Floor
+    {
+        public string name;
+        public Transform enemy;
+    }
+    public Floor[] floor;
     private Camera camera1;
 
-    private bool isMove;
-    private Vector3 destination;
+    private bool isClick;
+    private bool isMoving;
+    
+    private Vector2 destination;
     public float speed = 5;
 
-    public Rigidbody rb;
+    private Rigidbody2D rb;
 
-    Vector3 MousePosition;
+    
+    Vector2 MousePosition;
 
 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonUp(0))
         {
             MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            destination = new Vector3(MousePosition.x, transform.position.y,transform.position.z);
+            destination = new Vector2(MousePosition.x, transform.position.y);
             Debug.Log(destination);
-
-            isMove = true; //나중에 애니메이션 할때 필요하겠죠..?
-        }
-        if (isMove && transform.position != destination)
-        {
             
-            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
-            /*
-            if(transform.position.x > destination.x)
-            {
-                rb.AddForce(new Vector3(-1, 0, 0));
-            }
-            else rb.AddForce(new Vector3(1, 0, 0));
-            */
-
+            isClick = true; //애니메이션 할때 필요함
+        }
+        if (isClick && transform.position.x != destination.x)
+        {
+            isMoving = true;
+            isClick = false;
+            //transform.Translate(new Vector2(MousePosition.x - transform.position.x, 0)* Time.deltaTime * speed);
+            
         }
         else
         {
-            isMove = false;
+            isClick = false;
         }
-
-
-        //Move();
     }
-    /*
-    private void SetDestination(Vector3 dest)
+    private void FixedUpdate()
     {
-        destination = dest;
-        isMove = true;
-    }
-    private void Move()
-    {
-        if (isMove)
+        if (isMoving == true)
         {
-            var dir = destination - transform.position;
-            transform.forward = dir;
-            transform.position += dir.normalized * Time.deltaTime * 5f;
+            //transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+            //transform.position += (Vector3.right)*speed*Time.deltaTime; // 여기선 무조건 Vector3를 써야한다고함, 이거하면 계속 한쪽으로만 감
+            //rb.velocity = new Vector2(speed, 0); //질량을 고려하지 않고 움직임, y로 움직이지 않을 때 용이
+            //rb.AddForce(new Vector2(-speed, 0)); //질량을 고려하여 움직임
+            ///*
+            if (transform.position.x > destination.x)
+            {
+                rb.velocity = new Vector2(-speed, 0);//velocity를 사용하면 목적지에 다다랐을때 떨림
+                if (transform.position.x < destination.x)
+                {
+                    isMoving = false;
+                }
+            }
+            else
+            {
+                rb.velocity = new Vector2(speed, 0);
+                if(transform.position.x > destination.x)
+                {
+                    isMoving = false;
+                }
+            }
+            //*/
         }
-
-        if(Vector3.Distance(transform.position, destination) <= 0.1f)
+        
+        if(isMoving == false)
         {
-            isMove = false;
+            rb.AddForce(new Vector2(0, 0));
+            
         }
+        
+            //transform.position = new Vector2(0, 0);
     }
-    */
 }
