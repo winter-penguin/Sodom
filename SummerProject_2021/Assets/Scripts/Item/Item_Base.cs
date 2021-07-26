ea67eac240cfafffeab1504f0783cd7f5d166cbc
naeun0203 <jne02310@gmail.com>
2021-07-26 16:01:40 +0900

    Item_Base_Jne

--- SummerProject_2021/Assets/Scripts/Item/Item_Base.cs
+++ SummerProject_2021/Assets/Scripts/Item/Item_Base.cs
@@ -12,14 +12,13 @@ public class Item_Base : MonoBehaviour
         Cloth, Wood, Stone, Iron, Bonfire, Bed, Bag, Water_Purifier, Box
     };
     public ItemName CurrentItem = ItemName.Cloth;
-    public ItemType CurrentitemType = ItemType.Food;
-
+    public ItemType CurrentItemType = ItemType.Food;
+    private ItemName rayItemName;
+    private ItemType rayItemType;
     [SerializeField] protected float Hunger, Thirst, Heal, Fatigue, AD, Attack_Range, Capacity, Charge_Space;
     public Sprite itemImage;
     public GameObject itemPrefab;
-    private DBManager_Item_Food_Medicine itemData_Food_Medicine;
-    private DBManager_Item_Material itemData_Material;
-    private DBManager_Item_Object itemData_Object;
+    private DBManager_Item itemData;
 
     private bool useItem = false;
     // Start is called before the first frame update
@@ -29,9 +28,7 @@ public class Item_Base : MonoBehaviour
 
     void Start()
     {
-        itemData_Food_Medicine = GameObject.Find("DBManager").GetComponent<DBManager_Item_Food_Medicine>();
-        itemData_Material = GameObject.Find("DBManager").GetComponent<DBManager_Item_Material>();
-        itemData_Object = GameObject.Find("DBManager").GetComponent<DBManager_Item_Object>();
+        itemData = GameObject.Find("DBManager").GetComponent<DBManager_Item>();
         StartCoroutine(DataSet());
     }
     protected IEnumerator DataSet()
@@ -39,46 +36,46 @@ public class Item_Base : MonoBehaviour
         bool DataLoading = true;
         while (DataLoading)
         {
-            if (CurrentitemType == ItemType.Food || CurrentitemType == ItemType.Medicine)
+            if (CurrentItemType == ItemType.Food || CurrentItemType == ItemType.Medicine)
             {
-                for (int i = 0; i < itemData_Food_Medicine.itemDB.Length; i++)
+                for (int i = 0; i < itemData.itemDB_Food_Medicine.Length; i++)
                 {
 
-                    if (itemData_Food_Medicine.itemDB[i].name == CurrentItem.ToString())
+                    if (itemData.itemDB_Food_Medicine[i].name == CurrentItem.ToString())
                     {
-                        Hunger = itemData_Food_Medicine.itemDB[i].Hunger;
-                        Thirst = itemData_Food_Medicine.itemDB[i].Thirst;
-                        Heal = itemData_Food_Medicine.itemDB[i].Heal;
-                        Fatigue = itemData_Food_Medicine.itemDB[i].Fatigue;
-                        Charge_Space = itemData_Food_Medicine.itemDB[i].Charge_Space;
+                        Hunger = itemData.itemDB_Food_Medicine[i].Hunger;
+                        Thirst = itemData.itemDB_Food_Medicine[i].Thirst;
+                        Heal = itemData.itemDB_Food_Medicine[i].Heal;
+                        Fatigue = itemData.itemDB_Food_Medicine[i].Fatigue;
+                        Charge_Space = itemData.itemDB_Food_Medicine[i].Charge_Space;
                         DataLoading = false;
                     }
                 }
                 yield return null;
             }
-            else if(CurrentitemType == ItemType.Weapon || CurrentitemType == ItemType.Tool)
+            else if(CurrentItemType == ItemType.Weapon || CurrentItemType == ItemType.Tool)
             {
-                for (int i = 0; i < itemData_Material.itemDB.Length; i++)
+                for (int i = 0; i < itemData.itemDB_Material.Length; i++)
                 {
-                    if (itemData_Material.itemDB[i].name == CurrentItem.ToString())
+                    if (itemData.itemDB_Material[i].name == CurrentItem.ToString())
                     {
-                        AD = itemData_Material.itemDB[i].AD;
-                        Attack_Range = itemData_Material.itemDB[i].Attack_Range;
-                        Charge_Space = itemData_Material.itemDB[i].Charge_Space;
+                        AD = itemData.itemDB_Material[i].AD;
+                        Attack_Range = itemData.itemDB_Material[i].Attack_Range;
+                        Charge_Space = itemData.itemDB_Material[i].Charge_Space;
                         DataLoading = false;
                     }
                 }
                 yield return null;
             }
-            else if (CurrentitemType == ItemType.Ingredient || CurrentitemType == ItemType.Product)
+            else if (CurrentItemType == ItemType.Ingredient || CurrentItemType == ItemType.Product)
             {
-                for (int i = 0; i < itemData_Object.itemDB.Length; i++)
+                for (int i = 0; i < itemData.itemDB_Object.Length; i++)
                 {
-                    if (itemData_Object.itemDB[i].name == CurrentItem.ToString())
+                    if (itemData.itemDB_Object[i].name == CurrentItem.ToString())
                     {
-                        Fatigue = itemData_Object.itemDB[i].Fatigue;
-                        Capacity = itemData_Object.itemDB[i].Capacity;
-                        Charge_Space = itemData_Object.itemDB[i].Charge_Space;
+                        Fatigue = itemData.itemDB_Object[i].Fatigue;
+                        Capacity = itemData.itemDB_Object[i].Capacity;
+                        Charge_Space = itemData.itemDB_Object[i].Charge_Space;
                         DataLoading = false;
                     }
                 }
@@ -89,39 +86,58 @@ public class Item_Base : MonoBehaviour
     }
     void Update()
     {
-        if (CurrentitemType != ItemType.Ingredient)
+        if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Click");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject);
+                rayItemType = hit.transform.gameObject.GetComponent<Item_Base>().CurrentItemType;
+                rayItemName = hit.transform.gameObject.GetComponent<Item_Base>().CurrentItem;
+            }
+
             if (m_IsOneClick && ((Time.time - m_Timer) > m_DoubleClickSecond))
             {
-                Debug.Log("One Click");
                 m_IsOneClick = false;
             }
 
             if (Input.GetMouseButtonDown(0))
             {
-                if (!m_IsOneClick)
+                if (rayItemType != ItemType.Ingredient)
                 {
-                    m_Timer = Time.time;
-                    m_IsOneClick = true;
-                }
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (!m_IsOneClick)
+                        {
+                            m_Timer = Time.time;
+                            m_IsOneClick = true;
+                        }
+                    }
 
-                else if (m_IsOneClick && ((Time.time - m_Timer) < m_DoubleClickSecond))
-                {
-                    Debug.Log("Double Click");
-                    m_IsOneClick = false;
-                    useItem = true;
+                    else if (m_IsOneClick && ((Time.time - m_Timer) < m_DoubleClickSecond))
+                    {
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            Debug.Log("Double Click");
+                            m_IsOneClick = false;
+                            useItem = true;
+                        }
+                    }
                 }
             }
         }
-
         if (useItem)
         {
             UseItem();
         }
     }
 
+
     void UseItem()
     {
 
     }
+    
 }
