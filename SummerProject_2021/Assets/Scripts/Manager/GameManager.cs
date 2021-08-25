b35f2343b0a7a2558a6b05ed2828ecdc5c691188
naeun0203 <jne02310@gmail.com>
2021-08-25 12:39:21 +0900

    add-bool-isSecond_ing-jne

--- SummerProject_2021/Assets/Scripts/Manager/GameManager.cs
+++ SummerProject_2021/Assets/Scripts/Manager/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
 	// 게임이 끝났는가?
 	public bool isEnd;
 	public bool isUI;
+	public bool isSecond_ing;
 	private void Awake()
 	{
 		if (_instance != null && _instance != this)
