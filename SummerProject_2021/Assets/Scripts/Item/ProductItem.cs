c11388cf7812cc90933891cef86f8388b819c918
naeun0203 <jne02310@gmail.com>
2021-08-27 02:18:59 +0900

    produce-button-jne

--- SummerProject_2021/Assets/Scripts/Item/ProductItem.cs
+++ SummerProject_2021/Assets/Scripts/Item/ProductItem.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class ProductItem : MonoBehaviour
 {
+    public GameObject ProduceButton;
     public bool Product = false;
     public bool UseItem = false;
     private Item item;
     private ItemCraft Water;
     private GameObject box;
     private GameObject bonfire;
+    private float distance;
+    private GameObject Player;
     private void Start()
     {
         item = this.gameObject.GetComponent<Item>();
         Water = GameObject.Find("Water").GetComponent<ItemCraft>();
         box = GameObject.Find("MainUI").transform.Find("Box").gameObject;
         bonfire = GameObject.Find("MainUI").transform.Find("Bonfire").gameObject;
+        Player = GameObject.FindWithTag("Player");
     }
     private void Update()
     {
         if(UseItem)
         {
             StartCoroutine(CheckItem());
+        }
+        distance = Vector2.Distance(Player.transform.position, transform.position);
+        if (distance < 200)
+        {
+            ProduceButton.SetActive(true);
+        }
+        else
+        {
+            ProduceButton.SetActive(false);
         }
     }
     IEnumerator CheckItem()
